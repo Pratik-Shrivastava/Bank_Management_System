cccb650933b2d34892a84211e8af828cd3107cd8
Pratik-Shrivastava <pratikkumar441@gmail.com>
2023-09-11 00:54:22 +0530

    Added action listener and globalized some variables

--- src/bank/management/system/Login.java
+++ src/bank/management/system/Login.java
@@ -1,8 +1,18 @@
 package bank.management.system;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener {
+
+    //we need to define the buttons globally so that we can use them outside the constructor.
+    JButton login, signUp, clear;
+
+    //we also need to define the text-fields globally.
+    JTextField cardTextField;
+    //we want that our pin should not be visible, so we will use JPasswordField
+    JPasswordField pinTextField;
     Login()
     {
         //We have to set the title for the frame.
@@ -23,7 +33,7 @@ public class Login extends JFrame {
         //ClassLoader.getSystemResource() is used to pick image
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
 
-        //now we have to place the image on the frame, we cannot directly place a icon on frame.
+        //now we have to place the image on the frame, we cannot directly place an icon on frame.
         //JLabel label = new JLabel(i1);
         //add(label);
 
@@ -45,11 +55,11 @@ public class Login extends JFrame {
 
         //now we want to add some texts in the frame.
         JLabel text = new JLabel("Welcome to ATM");
-        //set the bounds of the text as we had change the layout to null
+        //set the bounds of the text as we had changed the layout to null
         text.setBounds(270, 40, 400, 40);
 
         //we want to change the font of the text.
-        text.setFont(new Font("Osward", Font.BOLD, 38));
+        text.setFont(new Font("Oswards", Font.BOLD, 38));
         add(text);
 
         JLabel cardNo = new JLabel("Card No:");
@@ -59,8 +69,9 @@ public class Login extends JFrame {
         cardNo.setBounds(120, 150, 150, 30);
         add(cardNo);
         //we want to add box
-        JTextField cardTextField = new JTextField();
+        cardTextField = new JTextField();
         cardTextField.setBounds(300,150,250,30);
+        cardTextField.setFont(new Font("Arial", Font.BOLD,18));
         add(cardTextField);
 
         JLabel pin = new JLabel("PIN:");
@@ -68,30 +79,57 @@ public class Login extends JFrame {
         pin.setBounds(120, 220, 150, 30);
         add(pin);
 
-        JTextField pinTextField = new JTextField();
+        pinTextField = new JPasswordField();
         pinTextField.setBounds(300,220,250,30);
+        pinTextField.setFont(new Font("Arial", Font.BOLD,18));
         add(pinTextField);
 
         //now we want to add buttons to the frame.
-        JButton login = new JButton("SIGN IN");
+        login = new JButton("SIGN IN");
         login.setBounds(300,300,100,30);
         login.setBackground(Color.BLACK);
         login.setForeground(Color.WHITE);
+        //now we have to add a method that will tell us that the button has been clicked.
+        login.addActionListener(this);
         add(login);
 
-        JButton clear = new JButton("CLEAR");
+        clear = new JButton("CLEAR");
         clear.setBounds(450,300,100,30);
         clear.setBackground(Color.BLACK);
         clear.setForeground(Color.WHITE);
+        //now we have to add a method that will tell us that the button has been clicked.
+        clear.addActionListener(this);
         add(clear);
 
-        JButton signUp = new JButton("SIGN UP");
+        signUp = new JButton("SIGN UP");
         signUp.setBounds(300,350,250,30);
         signUp.setBackground(Color.BLACK);
         signUp.setForeground(Color.WHITE);
+        //now we have to add a method that will tell us that the button has been clicked.
+        signUp.addActionListener(this);
         add(signUp);
 
 
+
+    }
+    public void actionPerformed(ActionEvent ae)
+    {
+        //When the button is clicked then what actions should be performed is given by this method.
+        //ae is the reference to an object of ActionEvent class.
+        //it has a method called getSource(), it tells us which button is clicked.
+        if(ae.getSource() == clear)
+        {
+            cardTextField.setText("");
+            pinTextField.setText("");
+        }
+        else if(ae.getSource() == login)
+        {
+
+        }
+        else if(ae.getSource() == signUp)
+        {
+
+        }
 
 
     }
