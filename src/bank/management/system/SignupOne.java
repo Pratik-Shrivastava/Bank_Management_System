8d8d200ecfac1c099403d334fbcd457787d04d1b
Pratik-Shrivastava <pratikkumar441@gmail.com>
2023-09-11 23:00:09 +0530

    Added TextFields for all the values

--- src/bank/management/system/SignupOne.java
+++ src/bank/management/system/SignupOne.java
@@ -12,15 +12,114 @@ public class SignupOne extends JFrame{
         long random = Math.abs((ran.nextLong() % 9000L + 1000L));
         getContentPane().setBackground(Color.WHITE);
 
-        JLabel formNo = new JLabel("APPLICATION FORM NO. "+random);
         setTitle("NEW ACCOUNT APPLICATION FORM - PAGE 2");
-        setSize(800, 480);
+        setSize(800, 700);
         setVisible(true);
         setLocation(350, 10);
-        JLabel head = new JLabel("Page 2: Additional Details");
         setLayout(null);
-        head.setFont(new Font("Oswards", Font.BOLD, 28));
-        add(head);
+
+        JLabel formNo = new JLabel("APPLICATION FORM NO. "+random);
+        formNo.setFont(new Font("Raleway", Font.BOLD, 38));
+        formNo.setBounds(140, 20, 600, 40);
+        add(formNo);
+
+
+        JLabel personalDetails = new JLabel("Page 1: Personal Details");
+        personalDetails.setFont(new Font("Raleway", Font.BOLD, 22));
+        personalDetails.setBounds(290, 60, 400, 30);
+        add(personalDetails);
+
+        JLabel name = new JLabel("Name:");
+        name.setFont(new Font("Raleway", Font.BOLD, 20));
+        name.setBounds(100, 140, 100, 30);
+        add(name);
+
+        JTextField nameTextField = new JTextField();
+        nameTextField.setFont(new Font("Raleway", Font.BOLD, 18));
+        nameTextField.setBounds(300, 140, 400, 30);
+        add(nameTextField);
+
+        JLabel fname = new JLabel("Father's Name:");
+        fname.setFont(new Font("Raleway", Font.BOLD, 20));
+        fname.setBounds(100, 190, 150, 30);
+        add(fname);
+
+        JTextField fnameTextField = new JTextField();
+        fnameTextField.setFont(new Font("Raleway", Font.BOLD, 18));
+        fnameTextField.setBounds(300, 190, 400, 30);
+        add(fnameTextField);
+
+        JLabel dob = new JLabel("Date of Birth:");
+        dob.setFont(new Font("Raleway", Font.BOLD, 20));
+        dob.setBounds(100, 240, 150, 30);
+        add(dob);
+
+        JLabel gender = new JLabel("Gender:");
+        gender.setFont(new Font("Raleway", Font.BOLD, 20));
+        gender.setBounds(100, 290, 150, 30);
+        add(gender);
+
+        JLabel email = new JLabel("Email:");
+        email.setFont(new Font("Raleway", Font.BOLD, 20));
+        email.setBounds(100, 340, 150, 30);
+        add(email);
+
+        JTextField emailTextField = new JTextField();
+        emailTextField.setFont(new Font("Raleway", Font.BOLD, 18));
+        emailTextField.setBounds(300, 340, 400, 30);
+        add(emailTextField);
+
+        JLabel marital = new JLabel("Marital Status:");
+        marital.setFont(new Font("Raleway", Font.BOLD, 20));
+        marital.setBounds(100, 390, 150, 30);
+        add(marital);
+
+        JLabel address = new JLabel("Address:");
+        address.setFont(new Font("Raleway", Font.BOLD, 20));
+        address.setBounds(100, 440, 150, 30);
+        add(address);
+
+        JTextField addressTextField = new JTextField();
+        addressTextField.setFont(new Font("Raleway", Font.BOLD, 18));
+        addressTextField.setBounds(300, 440, 400, 30);
+        add(addressTextField);
+
+        JLabel city = new JLabel("City:");
+        city.setFont(new Font("Raleway", Font.BOLD, 20));
+        city.setBounds(100, 490, 150, 30);
+        add(city);
+
+        JTextField cityTextField = new JTextField();
+        cityTextField.setFont(new Font("Raleway", Font.BOLD, 18));
+        cityTextField.setBounds(300, 490, 400, 30);
+        add(cityTextField);
+
+        JLabel state = new JLabel("State:");
+        state.setFont(new Font("Raleway", Font.BOLD, 20));
+        state.setBounds(100, 540, 150, 30);
+        add(state);
+
+        JTextField stateTextField = new JTextField();
+        stateTextField.setFont(new Font("Raleway", Font.BOLD, 18));
+        stateTextField.setBounds(300, 540, 400, 30);
+        add(stateTextField);
+
+        JLabel pincode = new JLabel("Pincode:");
+        pincode.setFont(new Font("Raleway", Font.BOLD, 20));
+        pincode.setBounds(100, 590, 150, 30);
+        add(pincode);
+
+        JTextField pincodeTextField = new JTextField();
+        pincodeTextField.setFont(new Font("Raleway", Font.BOLD, 18));
+        pincodeTextField.setBounds(300, 590, 400, 30);
+        add(pincodeTextField);
+
+
+
+
+
+
+
 
     }
     public static void main(String[] args) {
